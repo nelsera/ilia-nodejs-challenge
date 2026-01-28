29e52b35fc8d2430cf81caeeef24bdeb92f508b6
Nelson <franciscosilvanelson@gmail.com>
2026-01-27 21:42:11 -0300

    chore(user-service): add prisma schema and init migration

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserServiceController } from './user-service.controller';
 import { UserServiceService } from './user-service.service';
 import { MessagingModule } from './messaging/messaging.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MessagingModule],
+  imports: [MessagingModule, UsersModule, AuthModule],
   controllers: [UserServiceController],
   providers: [UserServiceService],
 })
